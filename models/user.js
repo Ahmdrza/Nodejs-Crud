397cd821fe641fb4361f51f7d4872734d8efafe6
Ahmad Raza <Ahmdrzalifa@gmail.com>
2019-01-14 20:41:18 +0500

    postgresql package added with example

--- models/user.js
+++ models/user.js
@@ -1,5 +1,18 @@
+const { Pool, Client } = require('pg')
+const connectionString = 'postgresql://postgres:admin@127.0.0.1:5432/node'
+
+const pool = new Pool({
+  connectionString: connectionString,
+})
+
 exports.count = function(req, res) {
+    
     return new Promise((resolve, reject) => {
-        setTimeout(() => reject(5), 3000)
+        pool.query('SELECT * FROM users WHERE user_id = $1', [1], (err, res) => {
+            if (err) {
+            throw err
+            }
+            resolve(res.rows[0]);
+        });
       });
 }
